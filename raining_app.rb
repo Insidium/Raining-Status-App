3bc083f9af3c0609e54aeadf6670fe60dd869207
Ryan Challen <ryanchallen@outlook.com>
2019-12-03 20:17:08 +1000

    Removed requirement text and checked logic for errors

--- raining_app.rb
+++ raining_app.rb
@@ -1,6 +1,5 @@
-You have access to two variables: raining (boolean) and temperature (integer). If it’s raining and the temperature is less than 15 degrees, print to the screen “It’s wet and cold”, if it is less than 15 but not raining  If it’s greater than or equal to 15 but not raining print “It’s warm but not raining”, and otherwise tell them “It’s warm and raining”.
 
-raining = false
+raining = true
 
 temperature = 0
 
